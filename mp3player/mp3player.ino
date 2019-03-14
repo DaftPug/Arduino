e44ad6cd4dfa165d413b49318d803c0eb7d73050
Michael Madsen <mm@pug-works.dk>
2019-03-14 14:54:18 +0100

    Created cube.ino - added mp3's and controls for button

--- mp3player/mp3player.ino
+++ mp3player/mp3player.ino
@@ -44,7 +44,7 @@ void setup()
   }
   Serial.println(F("DFPlayer Mini online."));
   
-  myDFPlayer.volume(10);  //Set volume value. From 0 to 30
+  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
   myDFPlayer.play(1);  //Play the first mp3
 }
 
